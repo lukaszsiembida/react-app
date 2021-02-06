2ce779051d1a3ef7f9e27250ec3152eddc1a6cb9
Uzytkownik <uzytkownik@kursGIT.pl>
2021-02-06 16:06:50 +0100

    'init'

---
+++ react-app/src/App/App.js
@@ -0,0 +1,15 @@
+import logo from '../logo.svg';
+import './App.css';
+import React from 'react';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                Hello {this.props.name + ' ' + this.props.surname} !
+            </div>
+        );
+    }
+}
+
+export default App;
