03fbcb3c4485f6c43e7fbea6f370e8c3607f0571
lukasz <lukaszsiembida@wp.pl>
2021-02-07 10:08:52 +0100

    first-commit

--- react-app/src/App/App.js
+++ react-app/src/App/App.js
@@ -1,10 +1,9 @@
-import logo from '../logo.svg';
 import './App.css';
 import React from 'react';
 
-class App extends React.Component{
-    render(){
-        return(
+class App extends React.Component {
+    render() {
+        return (
             <div>
                 Hello {this.props.name + ' ' + this.props.surname} !
             </div>
